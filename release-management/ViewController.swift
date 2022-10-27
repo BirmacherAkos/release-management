187adb9b1a28ab519ccdca7cc78417461b9ad1ac
Akos Birmacher <akos.birmacher@bitrise.io>
2022-10-27 12:35:23 +0200

    Initial Commit

---
+++ release-management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  release-management
+//
+//  Created by Birmacher Akos on 2022. 10. 27..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
